3458a4304bad98fbb8abaca887de26fe834ab668
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-03-26 07:54:23 +0530

    initial commit

---
+++ simple.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "You are running this script on self hosted Github runner.."
+echo "**********************************************************"
+echo "SELF HOSTED GITHUB RUNNER                                "
+echo "*********************************************************"
