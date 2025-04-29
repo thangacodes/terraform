931dec04327784c0e7bd832ce3588a2aa0a32088
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-04-29 21:00:22 +0530

    Add files via upload
    adding ec2 module files

---
+++ ec2-module/outputs.tf
@@ -0,0 +1,15 @@
+output "vmpubip" {
+  value = module.ec2.vmpubip
+}
+
+output "vmpvtip" {
+  value = module.ec2.vmpvtip
+}
+
+output "findtag" {
+  value = module.ec2.findtag
+}
+
+output "ec2-arn" {
+  value = module.ec2.ec2-arn
+}
