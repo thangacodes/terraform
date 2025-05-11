2ed789f19aba969f0859b738cc394ad8fb6b7022
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-05-11 11:18:42 +0530

    Update outputs.tf

--- alb-module/outputs.tf
+++ alb-module/outputs.tf
@@ -5,8 +5,12 @@ output "finding_pvtip" {
   value = module.vm.vmpvtip
 }
 
-output "alb_dns_name" {
-  value = module.app-alb.alb_dns_name
+output "http_alb_dns_name" {
+  value = module.app-alb.http_alb_dns_name
+}
+
+output "https_alb_dns_name" {
+  value = module.app-alb.https_alb_dns_name
 }
 
 output "alb_arn" {
@@ -23,4 +27,12 @@ output "instance_id_map" {
 
 output "tg-arn" {
   value = module.app-alb.tg-arn
+}
+
+output "web_server_public_ips" {
+  value = module.vm.web_server_public_ips
+}
+
+output "web_server_public_endpoint" {
+  value = module.vm.web_server_public_endpoint
 }
