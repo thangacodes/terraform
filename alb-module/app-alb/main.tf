ef47a6a2ab9b5d8b7851111ed0c3dc05dc57c251
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-05-11 09:39:52 +0530

    Update main.tf

--- alb-module/app-alb/main.tf
+++ alb-module/app-alb/main.tf
@@ -4,7 +4,6 @@ resource "aws_lb" "app_lb" {
   load_balancer_type = "application"
   security_groups    = var.security_groups
   subnets            = var.subnet_ids
-
   enable_deletion_protection = false
 }
 
@@ -13,7 +12,6 @@ resource "aws_lb_target_group" "app_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
   health_check {
     path                = "/"
     interval            = 30
@@ -28,7 +26,6 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_lb.arn
   port              = 80
   protocol          = "HTTP"
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
