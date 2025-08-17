83b82881e25b6fd0ccd7c6cc19743f2385278a49
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-08-17 21:57:08 +0530

    updated file

--- meta_argument/main.tf
+++ meta_argument/main.tf
@@ -13,33 +13,46 @@ provider "aws" {
   secret_key = "PROVIDE YOUR SECRET ACCESS KEY"
   region     = "ap-south-1"
 }
-
 resource "aws_vpc" "project-vpc" {
   cidr_block = "192.168.0.0/16"
   tags = {
     Name        = "dev-vpc"
     Environment = "development"
   }
-
 }
-
+resource "aws_subnet" "project-subnet" {
+  for_each = {
+    "app-subnet"      = "192.168.1.0/24"
+    "db-subnet"       = "192.168.2.0/24"
+    "bastion-subnet"  = "192.168.3.0/24"
+    "reserved-subnet" = "192.168.4.0/24"
+  }
+  vpc_id                  = aws_vpc.project-vpc.id
+  cidr_block              = each.value
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = each.key == "bastion-subnet" ? true : false
+  tags = {
+    Name     = each.key
+    Division = each.value
+  }
+  depends_on = [aws_vpc.project-vpc]
+}
 resource "aws_s3_bucket" "demo" {
   for_each = {
-    "dev"   = "gitops-devbucket-dev"
-    "stage" = "gitops-devbucket-stage"
-    "prod"  = "gitops-devbucket-prod"
+    "dev"   = "gitops-devbucket-log"
+    "stage" = "gitops-devbucket-log"
+    "prod"  = "gitops-devbucket-log"
   }
   bucket = "${each.key}-${each.value}"
   tags = {
     Environment = each.key
     bucket_name = each.value
   }
-  depends_on = [aws_vpc.project-vpc]
 }
 
 # Datatype toset in Terraform
 # resource "aws_iam_user" "projectusers" {
-#   for_each = to_set(["TomC", "SijuMP", "VijayKP"])
+#   for_each = to_set(["TomCruise", "Siju", "Vijay"])
 #   name     = each.key
 #   tags = {
 #     Name = each.key
@@ -50,14 +63,13 @@ resource "aws_s3_bucket" "demo" {
 ## # Datatype map in Terraform
 resource "aws_iam_user" "projectusers" {
   for_each = {
-    "developer" = "Vijay"
-    "devops"    = "Thangadurai"
-    "dba"       = "Anvika"
+    "developer" = "Simon"
+    "devops"    = "Steve"
+    "dba"       = "Smith"
   }
   name = each.value
   tags = {
     Name = each.value
     Role = each.key
   }
-  depends_on = [aws_vpc.project-vpc]
 }
