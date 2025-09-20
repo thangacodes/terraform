1823dfe1f42ada18aca27c2536b0078de6a66bff
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-09-20 21:25:13 +0530

    Update main.tf

--- OpenTofu/first_task/main.tf
+++ OpenTofu/first_task/main.tf
@@ -3,3 +3,9 @@ resource "local_file" "tofu" {
   content         = "Welcome to OpenTofu! It's easiest to learn it!!!"
   file_permission = "0700"
 }
+
+resource "local_sensitive_file" "cred" {
+  filename        = "./dbpassword.txt"
+  content         = "secrets"
+  file_permission = "0700"
+}
