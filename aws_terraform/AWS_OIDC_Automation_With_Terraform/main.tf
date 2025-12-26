9f8aa3076a603d4be9c5ce6e2ea9e442d83b8367
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-12-26 12:32:33 +0530

    initial commit

---
+++ aws_terraform/AWS_OIDC_Automation_With_Terraform/main.tf
@@ -0,0 +1,5 @@
+resource "aws_iam_openid_connect_provider" "oidc" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [var.oidc_thumbprint]
+}
