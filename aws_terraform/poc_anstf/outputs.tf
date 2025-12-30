2d38c6da75d462fcbfcc704c32bf10c83c073b87
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-12-30 21:32:09 +0530

    initial commit

---
+++ aws_terraform/poc_anstf/outputs.tf
@@ -0,0 +1,17 @@
+output "instance_name" {
+  value = { for key, value in aws_instance.nodes : key => value.tags.Name }
+}
+output "instance_pubip" {
+  value = { for key, value in aws_instance.nodes : key => value.public_ip }
+}
+output "instance_pvtip" {
+  value = { for key, value in aws_instance.nodes : key => value.private_ip }
+}
+
+output "jenkins_endpoint" {
+  value = "http://${aws_instance.nodes["jenkins"].public_ip}:8080/"
+}
+
+output "web_endpoint" {
+  value = "http://${aws_instance.nodes["web"].public_ip}:80/"
+}
